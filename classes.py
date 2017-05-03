10d1fff9b862d79e6042d57e14c112b223ba8f63
Joaquim Brotas Vasconcelos Esteves <ist177020@lab8p2.rnl.tecnico.ulisboa.pt>
2017-05-03 16:08:42 +0100

    pedestrians actuators done

--- classes.py
+++ classes.py
@@ -66,4 +66,51 @@ class Cars(object):
         self.speed = self.speed + 1
         drive(self)
     
-
+class Pedestrian(object):
+        def __init__(self, position,destination=settings.DEFAULT_DESTINATION,worldMap=WORLDMAP):
+            #speed: Return the speed the car is currently at
+            #position: Position of the car - ie: the patch it's on
+            self.position = position
+            #destination: Describes the patch the car wants to go to 
+            self.destination = destination
+            #current orientation: describes which way the car is oriented
+            self.current_orientation = [1,0]
+        
+        #sensors FIX ME
+        #actuators
+        def walk(self,orientation=self.current_orientation):
+            #Orientation should be something like
+            new_pos = [self.position[0] + orientation[0] * self.speed, self.position[1] + orientation[1] * self.speed]
+            
+            if not possiblePosition(new_pos):
+                raise PositionError("I am a person and I'm attempting to walk to impossible position {}".format(new_pos))
+            else:
+                self.position = new_pos
+                checkForCrash(new_pos,world_map)
+                return self.position
+    
+    
+        def turnLeft(self):
+            """turns the car to the left acording to his perspective, assume a birdseye view!
+                effectively [x,y] becomes [-y,x]
+            """
+            if self.current_orientation not in ACCEPTABLE_ORIENTATIONS:
+                raise OrientationError("I am a person and my orientation \"{}\" seems to be a bit wonky!".format(self.current_orientation))
+            else:
+                new_orientation = [  - self.current_orientation [1] , self.current_orientation[0] ]
+                return True
+            
+        def turnRight(self):
+            """turns the car to the right acording to his perspective, assume a birdseye view!
+                effectively [x,y] becomes [y,-x]
+            """
+            if self.current_orientation not in ACCEPTABLE_ORIENTATIONS:
+                raise OrientationError("I am a car and my orientation \"{}\" seems to be a bit wonky!".format(self.current_orientation))
+            else:
+                new_orientation = [ self.current_orientation [1] ,  - self.current_orientation[0] ]
+                return True
+            
+        def stop(self):
+            self.speed = 0
+        def accelerate(self):
+            self.speed = 1
